12ca6026b5f2a41715e6cd7a3b043c78aacb42ad
Ferry Dwianta P <72243035+ferry582@users.noreply.github.com>
2024-11-24 02:12:32 +0700

    add endpoint

---
+++ ios-bullion/Core/Utils/Network/AuthAPI.swift
@@ -0,0 +1,8 @@
+//
+//  AuthAPI.swift
+//  ios-bullion
+//
+//  Created by Ferry Dwianta P on 24/11/24.
+//
+
+import Foundation
---
+++ ios-bullion/Core/Utils/Network/Endpoint.swift
@@ -0,0 +1,8 @@
+//
+//  Endpoint.swift
+//  ios-bullion
+//
+//  Created by Ferry Dwianta P on 24/11/24.
+//
+
+import Foundation
